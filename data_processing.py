40ababffa872c50bd0855f6401725dea8d82d881
ishanmehta2 <ishanm@stanford.edu>
2024-02-14 21:00:24 -0800

    dp changes
    some more changes

--- data_processing.py
+++ data_processing.py
@@ -1,25 +1,27 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('sf.csv')
+sf_df = pd.read_csv('SF_weather.csv')
+ne_df = pd.read_csv('ne.csv')
+pats_data = pd.read_csv('pats_data.csv')
 
-<<<<<<< Updated upstream
-        datetime_value = df.iloc[i]['datetime']
-#         datetime_value = df.iloc[i]['datetime']
+ne_df['datetime'] = pd.to_datetime(ne_df['datetime']).dt.date
+pats_data['game_date'] = pd.to_datetime(pats_data['game_date']).dt.date
+
+ne_dates = set(ne_df['datetime'])
+pats_dates = set(pats_data['game_date'])
+
+# Find common dates
+common_dates = ne_dates.intersection(pats_dates)
+
+# Print common dates
+
+print(sorted(common_dates))
     
-        print(datetime_value)
-#         print(datetime_value)
-=======
-while True:
-    df['datetime'] = pd.to_datetime(df['datetime'])
 
-    # Sort the DataFrame by the 'datetime' column to ensure it's in chronological order
-    df.sort_values(by='datetime', inplace=True)
->>>>>>> Stashed changes
 
-    # Select every seventh day starting with the third day
-    # Since we start with the third day (index 2) and want every seventh day, we use slicing with a step of 7
-    selected_days = df.iloc[2::7]
-    print(selected_days['datetime'])
-    break
 
+
+
+
+    
