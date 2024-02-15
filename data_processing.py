7e6add2b7a73212f62b0a2d551aae16ca0165222
ishanmehta2 <ishanm@stanford.edu>
2024-02-14 20:44:42 -0800

    Update data_processing.py
    re-push

--- data_processing.py
+++ data_processing.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('weather.csv')
+df = pd.read_csv('sf.csv')
 
-for i in range(10):
-    # Get the value in the 'datetime' column for the ith row
-    if df.iloc[i]['icon'] == 'cloudy':
-    
-#         datetime_value = df.iloc[i]['datetime']
-    
-#         print(datetime_value)
-
+<<<<<<< Updated upstream
         datetime_value = df.iloc[i]['datetime']
 #         datetime_value = df.iloc[i]['datetime']
     
         print(datetime_value)
 #         print(datetime_value)
+=======
+while True:
+    df['datetime'] = pd.to_datetime(df['datetime'])
 
+    # Sort the DataFrame by the 'datetime' column to ensure it's in chronological order
+    df.sort_values(by='datetime', inplace=True)
+>>>>>>> Stashed changes
+
+    # Select every seventh day starting with the third day
+    # Since we start with the third day (index 2) and want every seventh day, we use slicing with a step of 7
+    selected_days = df.iloc[2::7]
+    print(selected_days['datetime'])
+    break
 
