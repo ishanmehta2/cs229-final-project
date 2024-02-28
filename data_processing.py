3fb9f73055489f16575142f0664e92a0ae651ad1
ishanmehta2 <ishanm@stanford.edu>
2024-02-27 23:39:52 -0800

    progress
    progress

--- data_processing.py
+++ data_processing.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import numpy as np
 
-sf_df = pd.read_csv('weather_data/sf.csv')
-ne_df = pd.read_csv('ne.csv')
-pats_data = pd.read_csv('pats_data.csv')
 
-ne_df['datetime'] = pd.to_datetime(ne_df['datetime']).dt.date
-pats_data['game_date'] = pd.to_datetime(pats_data['game_date']).dt.date
+# Initial Run
 
-ne_dates = set(ne_df['datetime'])
-pats_dates = set(pats_data['game_date'])
+# sf_df = pd.read_csv('weather_data/sf.csv')
+# ne_df = pd.read_csv('ne.csv')
+# pats_data = pd.read_csv('pats_data.csv')
 
-# Find common dates
-common_dates = ne_dates.intersection(pats_dates)
+# ne_df['datetime'] = pd.to_datetime(ne_df['datetime']).dt.date
+# pats_data['game_date'] = pd.to_datetime(pats_data['game_date']).dt.date
+
+# ne_dates = set(ne_df['datetime'])
+# pats_dates = set(pats_data['game_date'])
+
+# # Find common dates
+# common_dates = ne_dates.intersection(pats_dates)
 
 print("hello")
 # Dictionary definitions
@@ -22,10 +25,10 @@ feature_dict = {'play_id', 'game_id', 'home_team', 'away_team' 'yardline_100', '
                 'posteam_timeouts_remaining', 'defteam_timeouts_remaining', 'score_differential', 'roof', 'surface', 'offense_formation',
                 'offense_personnel', 'defenders_in_box', 'defense_personnel'}
 
-new_pats_data = pats_data[['play_id', 'game_id', 'home_team', 'away_team', 'yardline_100', 'game_date', 'game_seconds_remaining',
-                'down', 'ydstogo', 'play_type', 'no_huddle', 'shotgun', 'pass_length', 'pass_location', 'run_location',
-                'posteam_timeouts_remaining', 'defteam_timeouts_remaining', 'score_differential', 'roof', 'surface', 'offense_formation',
-                'offense_personnel', 'defenders_in_box', 'defense_personnel']]
+# new_pats_data = pats_data[['play_id', 'game_id', 'home_team', 'away_team', 'yardline_100', 'game_date', 'game_seconds_remaining',
+#                 'down', 'ydstogo', 'play_type', 'no_huddle', 'shotgun', 'pass_length', 'pass_location', 'run_location',
+#                 'posteam_timeouts_remaining', 'defteam_timeouts_remaining', 'score_differential', 'roof', 'surface', 'offense_formation',
+#                 'offense_personnel', 'defenders_in_box', 'defense_personnel']]
 
 # Format: {home team: weather.csv for home team}
 team_to_weather_mapping = {'ARI': 'weather_data/phoenix.csv', 'ATL' : 'weather_data/atlanta.csv', 'BAL' : 'weather_data/baltimore.csv', 
@@ -78,29 +81,129 @@ for team in teams:
         print(f'File not found for {team}, skipping.')
 
 
-print(weather_mapping)
+#print(weather_mapping)
 # print(team_data)
 
 # Updating weather to only be what we want it to be
 
-for team in cur_teams:
+#for team in cur_teams:
     # print(weather_mapping[team])
-    print(team_data[team])
+    #print(team_data[team])
 
+
+
+
+for team, df in team_data.items():
+    # Define the file name for each team's CSV
+    file_name = f"{team}.csv"
+    
+    # Save the DataFrame to CSV
+    df.to_csv(file_name, index=False)  # Set index=False if you don't want the index in the CSV
+
+print("CSV files have been saved for each team.")
 
 # print(team_data)
-for team in team_data:
-    game_loc = []
-    prev_row = None
-    for index, row in team_data.iterrows():
-        loc = row['home_team']
-        data = row['game_date']
-# Print common dates
 
-# print(sorted(common_dates))
 
-# print(pats_data.head())
     
+    # if team == 'ARI':
+        
+
+        # print(team_pd_df.loc[:, 'datetime'])
+        # Format: 2024-01-30
+
+# print(weather_mapping['ARI'].columns)
+
+
+
+for column in weather_mapping['ARI'].columns:
+    team_data['ARI'][column] = None
+    # print(team_data['ARI'].columns)
+
+for team in cur_teams:
+    team_vec = []
+    recent = 0
+    team_pd_df = team_data[team]
+    team_weather_pd_df = weather_mapping[team]
+
+    vectorized_team_dates= set()
+    # if team == 'ARI':
+    for i in range(1, len(team_pd_df)):
+        
+            vectorized_team_dates.add(team_pd_df['game_date'][i])
+
+    for ind in team_pd_df:
+        # for index in team_weather_pd_df:
+        for i in range(recent + 1, len(team_weather_pd_df)):
+            if team_weather_pd_df['datetime'][i] in vectorized_team_dates: 
+                team_vec.append(i)
+                recent = i
+    
+    cut_down_df = pd.DataFrame(columns=['name', 'datetime', 'tempmax', 'tempmin', 'temp', 'feelslikemax',
+       'feelslikemin', 'feelslike', 'dew', 'humidity', 'precip', 'precipprob',
+       'precipcover', 'preciptype', 'snow', 'snowdepth', 'windgust',
+       'windspeed', 'winddir', 'sealevelpressure', 'cloudcover', 'visibility',
+       'solarradiation', 'solarenergy', 'uvindex', 'severerisk', 'sunrise',
+       'sunset', 'moonphase', 'conditions', 'description', 'icon', 'stations'])
+    
+    weather_df_sorted = team_weather_pd_df.sort_values(by='datetime', ascending=True)
+    team_data_sorted = team_pd_df.sort_values(by='game_date', ascending=True)
+    # weather_df_sorted = weather_df_sorted.rename(columns={'datetime': 'game_date'})
+
+    
+    for ind in team_vec:
+    # Assuming 'ind' is a valid index in both DataFrames and they are aligned
+    # Extract row data from weather DataFrame
+        row_data = weather_df_sorted.loc[ind]
+        
+        # Update the team_data_sorted DataFrame's corresponding row
+        # Here, we iterate over each column that needs to be updated
+        for col in weather_df_sorted.columns:
+            # Assuming the same column names or you have a mapping of column names
+            # Update the value in team_data_sorted at the same index and column
+            if col in team_data_sorted.columns:
+                team_data_sorted.at[ind, col] = row_data[col]
+    
+    if team == 'ARI':
+        specific_date = '2016-09-11'
+        specific_row = team_data_sorted.query(f"game_date == '{specific_date}'")
+
+        # Print each column value for the row
+        if not specific_row.empty:
+            print(f"Values for {specific_date}:")
+            for column in specific_row.columns:
+                print(f"{column}: {specific_row.iloc[0][column]}")
+        else:
+            print(f"No data found for {specific_date}")
+
+
+            
+        
+
+
+    
+
+
+
+    
+            
+
+    
+
+
+    # print(team_vec)
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
